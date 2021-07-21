71ec0a6ccea090b5ff6d9f0751e44053eb4b6ca5
Simon Oxenford <oxenfordsimon@gmail.com>
2021-07-21 16:17:42 +0200

    fix open h5 file

--- RootMeanSquare/RootMeanSquare.py
+++ RootMeanSquare/RootMeanSquare.py
@@ -3,36 +3,35 @@
 import os
 import sys
 import numpy as np
-try:
-    import h5py
-except:
-    import slicer
-    slicer.util.pip_install('h5py')
-    import h5py
+import h5py
 
 def main(signal, samplingRate):
-    N = int(np.ceil(samplingRate * 50e-3)) # length of sub samples
-    signal = signal[int(np.remainder(len(signal),N)):] # cut signal so can be devided by N
-    X = np.reshape(signal, (-1,N)) # split signal into samples of length N and put them into matrix
-    rms = np.sqrt(np.mean(X**2, axis=1)) # root mean square value
-    rms_median = np.median(rms)
-    rms_std = np.std(rms)
-    stableParts = (rms < (rms_median+3*rms_std)) & (rms > (rms_median-3*rms_std))
-    edges = [0] + np.argwhere(np.logical_not(stableParts)).flatten().tolist() + [len(stableParts)]
-    longest = int(np.argmax(np.diff(edges)))
-    X = X[edges[longest]+3:edges[longest+1]-3][:]
-    stableSignal = X.flatten()
-    return np.sqrt(np.mean(stableSignal**2))
+  N = int(np.ceil(samplingRate * 50e-3)) # length of sub samples
+  signal = signal[int(np.remainder(len(signal),N)):] # cut signal so can be devided by N
+  X = np.reshape(signal, (-1,N)) # split signal into samples of length N and put them into matrix
+  rms = np.sqrt(np.mean(X**2, axis=1)) # root mean square value
+  rms_median = np.median(rms)
+  rms_std = np.std(rms)
+  stableParts = (rms < (rms_median+3*rms_std)) & (rms > (rms_median-3*rms_std))
+  edges = [0] + np.argwhere(np.logical_not(stableParts)).flatten().tolist() + [len(stableParts)]
+  longest = int(np.argmax(np.diff(edges)))
+  X = X[edges[longest]+3:edges[longest+1]-3][:]
+  stableSignal = X.flatten()
+  return np.sqrt(np.mean(stableSignal**2))
 
 
 if __name__ == "__main__":
-    dataFile = h5py.File(sys.argv[-1], 'r', libver='latest', swmr=True)
-    samplingRate = dataFile['sr'][:][0]
-    if (len(dataFile['data']) / samplingRate) < 4:
-        outValue = np.NaN
-    else:
-        outValue = main(dataFile['data'][:], samplingRate)
-    dataFile.close()
-    returnParameterFile = open(sys.argv[-2], "w")
-    returnParameterFile.write("rootMeanSquare = %.2f" % (outValue))
-    returnParameterFile.close()
+
+
+  try:
+    with h5py.File(sys.argv[-1], 'r', libver='latest', swmr=True) as dataFile:
+      signal = dataFile['data'][:]
+      samplingRate = dataFile['sr'][:][0]
+    outValue = main(signal, samplingRate)
+  except:
+    outValue = np.NaN
+
+
+  returnParameterFile = open(sys.argv[-2], "w")
+  returnParameterFile.write("rootMeanSquare = %.2f" % (outValue))
+  returnParameterFile.close()
